6cfe815dc7aa3ab22e792a0d335d6043c79701ea
maifeeulasad <maifeeulasad@gmail.com>
2021-04-19 17:55:52 +0600

    request permission

--- app/src/main/java/com/mua/filesbymaifee/MainActivity.kt
+++ app/src/main/java/com/mua/filesbymaifee/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.mua.filesbymaifee
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
@@ -7,17 +10,25 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.mua.filesbymaifee.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navHostFragment: NavHostFragment
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private var allPermissionGranted = true
+    private var requestCode = 1234
+
+    private var permissions = arrayOf(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-
         val binding = initBinding()
         initNav(binding)
+        initPermission()
     }
 
     private fun initBinding(): ActivityMainBinding {
@@ -39,4 +50,26 @@ class MainActivity : AppCompatActivity() {
             setupActionBarWithNavController(this, appBarConfiguration)
         }
     }
+
+    private fun initPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            requestPermissions(permissions, requestCode)
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == this.requestCode) {
+            grantResults.forEach { grantResult ->
+                if (!(grantResult == PackageManager.PERMISSION_GRANTED)) {
+                    allPermissionGranted = false
+                }
+            }
+        }
+    }
+
 }
